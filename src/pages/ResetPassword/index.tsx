b5c345b4073fdf7e90267c2265e403b706e78992
EnioGabriel <gabrielferreira.sc@gmail.com>
2021-07-17 03:50:59 -0300

    conectando redefinição de senha com o servidor

--- src/pages/ResetPassword/index.tsx
+++ src/pages/ResetPassword/index.tsx
@@ -3,7 +3,7 @@ import { FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 
 import { useToast } from '../../hooks/toast';
 import getValidationErrors from '../../utils/getValidatorErrors';
@@ -15,6 +15,7 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import { Container, Content, AnimationContainer, Background } from './styles';
+import api from '../../services/api';
 
 interface ResetPasswordFormData {
   password: string;
@@ -27,6 +28,7 @@ const ResetPassword: React.FC = () => {
   const { addToast } = useToast();
 
   const history = useHistory();
+  const location = useLocation();
 
   const handleSubmit = useCallback(
     async (data: ResetPasswordFormData) => {
@@ -43,6 +45,21 @@ const ResetPassword: React.FC = () => {
 
         await schema.validate(data, {
           abortEarly: false, // retorna todos os erros de uma vez só
+        });
+
+        const { password, password_confirmation } = data;
+
+        // pegando o token passado no param e formatando para pegar apenas o token puro
+        const token = location.search.replace('?token=', '');
+
+        if (!token) {
+          throw new Error();
+        }
+
+        await api.post('/password/reset', {
+          password,
+          password_confirmation,
+          token,
         });
 
         history.push('/');
@@ -62,7 +79,7 @@ const ResetPassword: React.FC = () => {
         });
       }
     },
-    [addToast, history],
+    [addToast, history, location],
   );
 
   return (
