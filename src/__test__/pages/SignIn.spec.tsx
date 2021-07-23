cd6acdcba7642d8b6aefefe9cdfcb1dc6b38f61c
EnioGabriel <gabrielferreira.sc@gmail.com>
2021-07-23 01:38:27 -0300

    teste de login

--- src/__test__/pages/SignIn.spec.tsx
+++ src/__test__/pages/SignIn.spec.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import SignIn from '../../pages/SignIn';
+
+const mockedHistoryPush = jest.fn();
 
 jest.mock('react-router-dom', () => {
   return {
-    useHistory: jest.fn(),
+    useHistory: () => ({
+      push: mockedHistoryPush,
+    }),
     Link: ({ children }: { children: React.ReactNode }) => children,
   };
 });
@@ -13,12 +16,20 @@ jest.mock('react-router-dom', () => {
 // describe indica qual categoria do teste
 describe('SignIn Page', () => {
   it('should be able to sign in', () => {
-    const { debug } = render(
-      // <Router>
-      <SignIn />,
-      // </Router>,
-    );
+    const { getByPlaceholderText, getByText } = render(<SignIn />);
 
-    debug();
+    const emailField = getByPlaceholderText('E-mail');
+    const passwordField = getByPlaceholderText('Senha');
+    const buttonElement = getByText('Entrar');
+
+    // fireEvent: simula uma interação do usuário com a tela
+    fireEvent.change(emailField, {
+      target: { value: 'fulanodetal@gmail.com' },
+    });
+    fireEvent.change(passwordField, { target: { value: 'fulanodetalSenha' } });
+
+    fireEvent.click(buttonElement);
+
+    expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard');
   });
 });
