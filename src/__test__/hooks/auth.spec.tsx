db01e3db14561542c446be41add8954edec8927a
EnioGabriel <gabrielferreira.sc@gmail.com>
2021-07-27 16:09:56 -0300

    criando mock da API

--- src/__test__/hooks/auth.spec.tsx
+++ src/__test__/hooks/auth.spec.tsx
@@ -1,11 +1,28 @@
 import { renderHook } from '@testing-library/react-hooks';
-import { AuthProvider, useAuth } from '../../hooks/auth';
+import MockAdapter from 'axios-mock-adapter';
 
-// jest.mock();
+import { AuthProvider, useAuth } from '../../hooks/auth';
+import api from '../../services/api';
+
+const apiMock = new MockAdapter(api);
 
 describe('Auth hook', () => {
-  it('should be able to sign in', () => {
-    const { result } = renderHook(() => useAuth(), {
+  it('should be able to sign in', async () => {
+    const apiResponse = {
+      user: {
+        id: 'userId',
+        name: 'fulano',
+        email: 'fulano@gmail.com',
+      },
+      token: 'token-123',
+    };
+
+    apiMock.onPost('sessions').reply(200, apiResponse);
+
+    // Verifica se a função setItem do localStorage foi disparada
+    const setItemSpy = jest.spyOn(Storage.prototype, 'setItem');
+
+    const { result, waitForNextUpdate } = renderHook(() => useAuth(), {
       wrapper: AuthProvider,
     });
 
@@ -13,6 +30,18 @@ describe('Auth hook', () => {
       email: 'fulano@gmail.com',
       password: '123456',
     });
+
+    // Aguarda mudança de estado do hook
+    await waitForNextUpdate();
+
+    expect(setItemSpy).toHaveBeenCalledWith(
+      '@GoBarber:token',
+      apiResponse.token,
+    );
+    expect(setItemSpy).toHaveBeenCalledWith(
+      '@GoBarber:user',
+      JSON.stringify(apiResponse.user),
+    );
 
     expect(result.current.user.email).toEqual('fulano@gmail.com');
   });
